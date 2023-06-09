e60ff2a1c4e92bbe8b122e2f713a934a36ebc08a
youjin kim <gracecancho@gmail.com>
2023-06-09 17:57:23 +0900

    dashboard

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {
-    getProducts,
-    getCustomers,
-    getTransactions,
-    getGeography,
+  getProducts,
+  getCustomers,
+  getTransactions,
+  getGeography,
 } from "../controllers/client.js";
 
 const router = express.Router();
